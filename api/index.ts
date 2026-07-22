91e3664a89f61470cec840671b9a21aeacf0ab0f
DeeEmperor <deeemperor101@gmail.com>
2026-07-22 17:39:41 +0100

    use explicit functions config, remove framework:null

--- api/index.ts
+++ api/index.ts
@@ -2,4 +2,6 @@
 // This re-uses the same Express app used for local development.
 import app from "../server/app";
 
+console.log("[BizedExclusive API] Function loaded at", new Date().toISOString());
+
 export default app;
